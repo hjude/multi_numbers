064aaab4713a02cebd9c160b2b27d8564d48504d
John Pro <hjudey@gmail.com>
2023-03-30 14:34:15 -0400

    Create fast_way.py

---
+++ fast_way.py
@@ -0,0 +1,3 @@
+def is_multiple(x): # x can be any number, integer or float type
+    if (len([y for y in range(-10**9+2)]) == len((-3*i + j)*j/4 - i * (-5*(n-k))/(8.76^d))) and not is_multiple(x): # x must be multiple of
+        return True                              # if it's a prime number, then print the value in the list "prime"   else:     False
